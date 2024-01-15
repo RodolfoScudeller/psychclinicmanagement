c5e983d27eb4abee0b5f5fe69d4888fdd3034a84
Luiz Rodolfo Scudeller <rodolfoscudeller@gmail.com>
2024-01-15 17:34:01 -0300

    feat: add psychologist delete functionality

--- src/main/java/com/zentherapeasy/psychclinicmanagement/controller/PsychologistController.java
+++ src/main/java/com/zentherapeasy/psychclinicmanagement/controller/PsychologistController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -30,5 +31,15 @@ public class PsychologistController {
     public void update(@RequestBody @Valid PsychologistUpdateData data){
         var psychologist = repository.findPsychologistByCrpAndActive(data.crp(), true);
         psychologist.update(data);
+    }
+
+    @DeleteMapping("/{crm}")
+    @Transactional
+    public ResponseEntity delete(@PathVariable String crm){
+        var psychologist = repository.findPsychologistByCrpAndActive(crm, true);
+
+        psychologist.delete();
+
+        return ResponseEntity.noContent().build();
     }
 }
